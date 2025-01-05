72310ae80c1c1ceb3d0e8a421914958181fed2bb
Michael Washington <webmaster@adefwebserver.com>
2025-01-04 16:56:12 -0800

    Add project files.

---
+++ BlazorRDLCViewerWebAssembly.Client/Reports/ReportItem.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorRDLCViewerWebAssembly
+{
+    public class ReportItem
+    {
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int Qty { get; set; }
+        public decimal Total { get; set; }
+    }
+}
